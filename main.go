33760734078e9d79b1f5f9d26156ce29116933ad
buckwheattN1 <grykajm@gmail.com>
2022-10-30 19:51:44 +0100

    Split for multiply pages

--- main.go
+++ main.go
@@ -3,18 +3,23 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
-func handlerFunc(w http.ResponseWriter, r *http.Request) {
+func home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	if r.URL.Path == "/" {
-		fmt.Fprint(w, "Home Page")
-	} else if r.URL.Path == "/contact" {
-		fmt.Fprint(w, "Contact Page")
-	}
+	fmt.Fprint(w, "Homedd Page")
+}
+
+func contact(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "Contact Page")
 }
 
 func main() {
-	http.HandleFunc("/", handlerFunc)
-	http.ListenAndServe(":3000", nil)
+	r := mux.NewRouter()
+	r.HandleFunc("/", home)
+	r.HandleFunc("/contact", contact)
+	http.ListenAndServe(":3000", r)
 }
