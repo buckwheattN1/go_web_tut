c8190ba616537f4267bc4a3bf6459f618903b7c1
buckwheattN1 <grykajm@gmail.com>
2022-10-30 18:34:06 +0100

    Simple routering

--- main.go
+++ main.go
@@ -1,11 +1,17 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 )
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
+	if r.URL.Path == "/" {
+		fmt.Fprint(w, "Home Page")
+	} else if r.URL.Path == "/contact" {
+		fmt.Fprint(w, "Contact Page")
+	}
 }
 
 func main() {
