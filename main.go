c776e2d764490d01f561e23908ce8b7eb8ce113c
buckwheattN1 <grykajm@gmail.com>
2022-10-31 18:08:18 +0100

    add faq and 404 page

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 
 func home(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
-	fmt.Fprint(w, "Homedd Page")
+	fmt.Fprint(w, "<h1>Homedd Page</h1>")
 }
 
 func contact(w http.ResponseWriter, r *http.Request) {
@@ -17,9 +17,21 @@ func contact(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "Contact Page")
 }
 
+func faq(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "FAQ page")
+}
+
+func Handle404(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "<p>404 error, page not found. Go back to <a href=\"/\">main page</a></p>")
+}
+
 func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/", home)
 	r.HandleFunc("/contact", contact)
+	r.HandleFunc("/faq", faq)
+	r.NotFoundHandler = http.HandlerFunc(Handle404)
 	http.ListenAndServe(":3000", r)
 }
